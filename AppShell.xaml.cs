b020756b4bc1058ad1b3beaf4d4554bccbe700db
231025 <231025@CPD03-M04>
2025-06-02 11:27:59 -0300

    Adicionar arquivos de projeto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppTempoAgoraSQlite
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
